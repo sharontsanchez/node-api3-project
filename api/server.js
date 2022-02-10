59ea2f8490b271ce14990a99fc61c313d8ba4d39
Sharon Sanchez <sstharakan@gmail.com>
2022-02-09 21:38:22 -0600

    the logger logs to the console with the method, url and timestamp request and runs on every request

--- api/server.js
+++ api/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
+const {logger} = require('./middleware/middleware');
 const server = express();
 
 server.use(express.json())
-
+server.use(logger)
 // remember express by default cannot parse JSON in request bodies
 
 // global middlewares and the user's router need to be connected here
